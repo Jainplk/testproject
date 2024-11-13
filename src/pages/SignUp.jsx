6da4ad24424d3ad3d4f38aa7a66def99c6d3f31d
Palak Jain <160907087+Jainplk@users.noreply.github.com>
2024-11-13 18:05:18 +0530

    Add files via upload

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignupNavbar from '../componenets/SignupComponents/SignupNavbar';
+import SignupPage from '../componenets/SignupComponents/SignupPage';
+
+ function SignUp() {
+    return ( 
+       <>
+        <SignupNavbar/>
+        <SignupPage/>
+       </>
+     );
+ }
+ 
+ export default SignUp;
